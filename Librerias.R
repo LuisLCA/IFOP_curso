045b1af59ea8504376ad885ff0eb41c833f2f1a7
Luis La Cruz <lacruz.luis2886@gmail.com>
2023-08-11 10:58:26 -0400

    commit inicial

---
+++ Librerias.R
@@ -0,0 +1,20 @@
+#Instalar git
+#https://git-scm.com/
+
+library(usethis)
+
+
+#2. Crear Token
+create_github_token()
+
+#Codigo generado
+#Almacenar credenciales
+#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
+
+install.packages("gitcreds")
+library(gitcreds)
+
+gitcreds::gitcreds_set()
+
+#copiar el token: 
+#  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
